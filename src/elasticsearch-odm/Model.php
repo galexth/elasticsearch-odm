a79a7c69c4b6ae3108a431e1e66f9af45ca5f363
Alexander Gorlov <alexander.g.gorlov@gmail.com>
2018-10-29 17:10:08 +0700

    changed toArray function

--- src/elasticsearch-odm/Model.php
+++ src/elasticsearch-odm/Model.php
@@ -1258,7 +1258,9 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
      */
     public function toArray()
     {
-        return array_merge($this->attributesToArray(), $this->relationsToArray());
+        return $this->attributesToArray() + [
+            'relations' => $this->relationsToArray()
+        ];
     }
 
     /**
